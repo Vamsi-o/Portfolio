f1524c18727a2e608efca6da34ce15c6ec987c4a
Vamsi_0 <iamvamsi0@gmail.com>
2025-10-25 20:23:03 +0530

    Doing Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import HeroSection from "./components/Hero-Section";
 import NavBar from "./components/NavBar";
 import Projects from "./components/Projects";
@@ -5,10 +6,11 @@ import Projects from "./components/Projects";
 export default function Home() {
   return (
     <>
-      <div className="w-screen min-h-screen ">
+      <div className="w-screen min-h-screen bg-[#090a15] ">
         {/* <NavBar /> */}
         <HeroSection />
-        
+        <Footer/>
+
 
       </div>
 
