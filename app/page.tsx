2a50a79fdc87afdf44a440e7c7a316ed26f06e68
Vamsi_0 <iamvamsi0@gmail.com>
2025-11-20 22:23:00 +0530

    Styling Done

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ export default function Home() {
       {/* About Section */}
       <About />
       
-      {/* Projects Section */}
       <Projects />
       
       {/* Footer with Contact Form */}
